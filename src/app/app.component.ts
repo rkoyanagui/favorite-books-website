bec6958f322c3fc2c84cf705a37ada91174e06fd
Rubem Koyanagui <39101149+rkoyanagui@users.noreply.github.com>
2022-10-28 00:18:06 -0300

    Static book list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'favorite-books-website';
+
+  languageFilter: 'all' | 'en' | 'pt' | 'fr' = 'all';
+
+  allBooks = [
+    {
+      title: 'Moby-Dick',
+      author: 'Melville, Herman',
+      language: 'en',
+    },
+    {
+      title: 'Paradise Lost',
+      author: 'Milton, John',
+      language: 'en',
+    },
+    {
+      title: 'Le Crime de Sylvestre Bonnard',
+      author: 'France, Anatole',
+      language: 'fr',
+    },
+    {
+      title: 'Star Maker',
+      author: 'Stapledon, Olaf',
+      language: 'en',
+    },
+  ];
+
+  get books() {
+    if (this.languageFilter === 'all') {
+      return this.allBooks;
+    }
+    return this.allBooks.filter((book) => book.language === this.languageFilter);
+  }
 }
